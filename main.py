1fd4d9947ec29a9b0d58fd91569116e2e3535304
James Brunet <james@brunet.dev>
2024-02-02 13:04:31 -0500

    Add sunglasses emoji

--- main.py
+++ main.py
@@ -3,4 +3,4 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 name = input("What is your name? ")
-print(f"Hello {name}!")
+print(f"Hello {name}! 😎")
